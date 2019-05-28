8f4814aaedb9f09b567b2033eac43e1dfc4799e7
Sam Markham <sam.e.markham@gmail.com>
2019-05-28 22:00:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,9 @@ def first_challenge
   }
 
   #your code here
-  contacts.each do 
+  contacts.each do |person, data|
+    data.each do |attribute, value| 
+      if attribute = :favorite_icecream_flavors
 
   #remember to return your newly altered contacts hash!
   contacts
